92994b634edf1738c29c55fa160cf525ee8a4e6f
gabrielsong9674 <gracegabrielson@icloud.com>
2017-12-18 16:17:28 -0800

    upl 1

---
+++ FrcProject_01/src/team3647robotPackage/Motors.java
@@ -0,0 +1,8 @@
+package team3647robotPackage;
+
+import edu.wpi.first.wpilibj.Spark;
+
+public class Motors {
+	public static Spark leftMotor = new Spark(2);
+	public static Spark rightMotor = new Spark(1);
+}
