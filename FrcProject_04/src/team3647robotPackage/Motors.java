aec32b2e8a99ae9bbfb57062d621709ed9287fbf
gabrielsong9674 <gracegabrielson@icloud.com>
2018-01-15 16:32:31 -0800

    upload

--- FrcProject_04/src/team3647robotPackage/Motors.java
+++ FrcProject_04/src/team3647robotPackage/Motors.java
@@ -1,6 +1,8 @@
 package team3647robotPackage;
 
-public class Motors 
-{
+import edu.wpi.first.wpilibj.Spark;
 
+public class Motors {
+	public static Spark leftMotor = new Spark(2);
+	public static Spark rightMotor = new Spark(1);
 }
