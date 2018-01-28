14db64270e3e8269274582d92a64ed0e3b7e63f0
gabrielsong9674 <gracegabrielson@icloud.com>
2018-01-27 18:24:14 -0800

    update

--- FrcProject_05/src/org/usfirst/frc/team3647/robot/Motors.java
+++ FrcProject_05/src/org/usfirst/frc/team3647/robot/Motors.java
@@ -1,27 +1,12 @@
 package org.usfirst.frc.team3647.robot;
 
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.Spark;
 
 public class Motors 
 {
-	public static WPI_TalonSRX leftSRX = new WPI_TalonSRX(0);
-	public static WPI_TalonSRX rightSRX = new WPI_TalonSRX(3);
-	
-	public static VictorSPX leftSPX1 = new VictorSPX(1);
-	public static VictorSPX rightSPX1 = new VictorSPX(0);
-	public static VictorSPX leftSPX2 = new VictorSPX(2);
-	public static VictorSPX rightSPX2 = new VictorSPX(3);
-	
-	public static void drivetrainInitialization()
-	{
-		Motors.leftSRX.set(0);
-		Motors.rightSRX.set(0);
-		leftSPX1.follow(leftSRX);
-		leftSPX2.follow(leftSRX);    
-		rightSPX1.follow(rightSRX);
-		rightSPX2.follow(rightSRX);
-	}
-	
-	public static DifferentialDrive drive = new DifferentialDrive(Motors.leftSRX, Motors.rightSRX);
+//	0,1 right
+//	2,3 left
+	public static Spark leftSpark = new Spark(2);
+	public static Spark rightSpark = new Spark(1);
 	
 }
